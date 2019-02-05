18a46333f52219750f61e81c8c5630aa9f8dd711
galyna <galynavistovska@gmail.com>
2019-02-05 14:58:56 +0200

    var to const and let

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
-var express = require('express');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
 
-const MongoClient    = require('mongodb').MongoClient;
-var db = {url:"mongodb://localhost:27017/notes"};
+const db = {url: "mongodb://localhost:27017/notes"};
 
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-
-var app = express();
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 MongoClient.connect(db.url, (err, database) => {
-    if (err) return console.log(err)
-
+    if (err) return console.log(err);
     require('./routes')(app, database);
-
-    app.listen(8000, () => {
-        console.log('We are live on ' + 8000);
-    });
-
-})
+});
 
 module.exports = app;
