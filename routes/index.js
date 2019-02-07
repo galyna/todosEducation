d01b1c45627c776945416a68d83205705d7cfe67
galyna <galynavistovska@gmail.com>
2019-02-07 17:18:09 +0200

    url fixes

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,39 @@
 module.exports = function (app, database) {
-    let ObjectID = require("mongodb").ObjectID;
+    const ObjectID = require("mongodb").ObjectID;
     const notesURL = "/notes";
     const collection = database.collection("notes");
-    app.get(notesURL + `/:id`, (req, res) => {
-        const id = req.params.id;
-        const details = {"_id": new ObjectID(id)};
-        collection.findOne(details, (err, item) => {
-            if (err) {
-                res.send({"error": "An error has occurred"});
-            } else {
-                res.send(item);
-            }
-        });
-    });
-    app.get(notesURL, (req, res) => {
-        collection.find().toArray((err, result) => {
-            if (err) {
-                res.send({"error": "An error has occurred"});
-            } else {
-                res.send(result);
-            }
-        });
-    });
-    app.post(notesURL, (req, res) => {
-        const note = {text: req.body.body, title: req.body.title};
-        collection.insertOne(note, (err, result) => {
-            if (err) {
-                res.send({"error": "An error has occurred"});
-            } else {
-                res.send(result.ops[0]);
-            }
-        });
-    });
-    app.put(notesURL + `/:id`, (req, res) => {
-        const id = req.params.id;
-        const details = {"_id": new ObjectID(id)};
-        const note = {text: req.body.body, title: req.body.title};
-        collection.updateOne(details, note, (err, result) => {
-            if (err) {
-                res.send({"error": "An error has occurred"});
-            } else {
-                res.send(note);
-            }
-        });
-    });
-    app.delete(notesURL + `/:id`, (req, res) => {
-        const id = req.params.id;
-        const details = {"_id": new ObjectID(id)};
-        collection.removeOne(details, (err, item) => {
-            if (err) {
-                res.send({"error": "An error has occurred"});
-            } else {
-                res.send("Note " + id + " deleted!");
-            }
-        });
-    });
+
+    const asyncMiddleware = fn =>
+        (req, res, next) => {
+            Promise.resolve(fn(req, res, next))
+                .catch(next);
+        };
+
+    app.get(notesURL + `/:id`, asyncMiddleware(async (req, res, next) => {
+        const details = {"_id": new ObjectID(req.params.id)};
+        const item = await collection.findOne(details);
+        res.send(item);
+    }));
+    app.get(notesURL, asyncMiddleware(async (req, res, next) => {
+        const result = await collection.find().toArray();
+        res.send(result);
+    }));
+    app.post(notesURL, asyncMiddleware(async (req, res, next) => {
+        const note = {body: req.body.body, title: req.body.title};
+        const result = await collection.insertOne(note);
+        res.send(result.ops[0]);
+    }));
+    app.put(notesURL + `/:id`, asyncMiddleware(async (req, res, next) => {
+        const result = await collection.findOneAndUpdate(
+            {"_id": new ObjectID(req.params.id)},
+            {$set: {"body": req.body.body, "title": req.body.title}},
+            {returnOriginal: false})
+        res.send(result.value);
+    }));
+    app.delete(notesURL + `/:id`, asyncMiddleware(async (req, res, next) => {
+        const details = {"_id": new ObjectID(req.params.id)};
+        await collection.removeOne(details)
+        res.send("Note " + req.params.id + " deleted!");
+    }));
 };
 
